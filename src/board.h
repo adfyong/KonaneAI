b20bf650c631edea320153b386f6e34863218bcf
Tristan Bergstra <bergstrat@students.cs.macewan.ca>
2016-11-01 16:05:05 -0600

    fixes header files, removes main.h, removes functions from main.c

--- src/board.h
+++ src/board.h
@@ -4,9 +4,6 @@
 #include <inttypes.h>
 
 void printBoard(uint64_t board);
-
 int getType(int argc);
-
 uint64_t *getMoves(char argc, uint64_t board);
-
 uint64_t *getPieceMoves(int type, int spot, uint64_t board);
--- src/main.h
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <inttypes.h>
-#include "board.h"

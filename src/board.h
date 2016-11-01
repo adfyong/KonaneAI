a0584a60c2c2eaf4ac87aab97e05af773e686b64
Tristan Bergstra <bergstrat@students.cs.macewan.ca>
2016-11-01 15:33:04 -0600

    adds functions to take board input, print board and getting the current avaialable moves

---
+++ src/board.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+void printBoard(uint64_t board);
+
+int getType(int argc);
+
+uint64_t *getMoves(char argc, uint64_t board);
+
+uint64_t *getPieceMoves(int type, int spot, uint64_t board);

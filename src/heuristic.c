bf2b15f687cc07706244b3d74fb908c28cafc40b
Alan Yong <yonga@mymacewan.ca>
2016-11-27 11:45:32 -0700

    Fix bug in heuristic, amplify game_over states

--- src/heuristic.c
+++ src/heuristic.c
@@ -2,7 +2,14 @@
 #include <stdio.h>
 #include <inttypes.h>
 
+#ifdef DEBUG_HEUR
+#include "board.h"
+#endif
+
 #define IS_BLACK(i) ((((i/8) % 2) && (i % 2)) || (!((i/8) % 2) && !(i % 2)))
+
+#define POS_NUM(i) (i/8 + 1)
+#define POS_LETTER(i) (7 - (i%8) + 'a')
 
 /* For player, detect if there are no more moves remaining */
 // TODO TODO TODO: this is doing twice as much work as necessary
@@ -60,41 +67,33 @@ int count_movable(uint64_t board) {
 	int b_movable = 0;
 	uint64_t o = 1; // a 64-bit version of 1 so we can shift it
 #ifdef DEBUG_HEUR
-	fprintf(stderr, "Heuristic analyzing ");
-	printf("%" PRIx64 "\n", board);
+	fprintf(stderr, "Heuristic analyzing\n");
+	printBoard(board);
 #endif
 	for (int i = 0; i < 64; ++i) {
-		if (!(board & (1 << i)))
+		if (!(board & (o << i)))
 			// bit is unset
 			continue;
 
-		// up
-		if (i < 48 && (board & (o << (i+8))) && !(board & (o << (i+16)))) {
+		// up, down, left, right
+		if ((i < 48 && (board & (o << (i+8))) && !(board & (o << (i+16)))) ||
+			(i > 15 && (board & (o << (i-8))) && !(board & (o << (i-16)))) ||
+			((i%8 > 1) && (board & (o << (i-1))) && !(board & (o << (i-2)))) ||
+			((i%8 < 5) && (board & (o << (i+1))) && !(board & (o << (i+2))))) {
+#ifdef DEBUG_HEUR
+			fprintf(stderr, "%c%d (bit %d) is movable\n", POS_LETTER(i), POS_NUM(i), i);
+#endif
 			IS_BLACK(i) ? b_movable++ : w_movable++;
-			continue;
-		}
-
-		// down
-		if (i > 15 && (board & (o << (i-8))) && !(board & (o << (i-16)))) {
-			IS_BLACK(i) ? b_movable++ : w_movable++;
-			continue;
-		}
-
-		// right
-		if ((i%8 > 1) && (board & (o << (i-1))) && !(board & (o << (i-2)))) {
-			IS_BLACK(i) ? b_movable++ : w_movable++;
-			continue;
-		}
-
-		// left
-		if ((i%8 < 5) && (board & (o << (i+1))) && !(board & (o << (i+2)))) {
-			IS_BLACK(i) ? b_movable++ : w_movable++;
-			continue;
 		}
 	}
+
+	int h = (b_movable == 0 ? -50 : b_movable) -
+		(w_movable == 0 ? -50 : w_movable);
+
 #ifdef DEBUG_HEUR
 	fprintf(stderr, "Movable black pieces: %d\n", b_movable);
 	fprintf(stderr, "Movable white pieces: %d\n", w_movable);
+	fprintf(stderr, "Heuristic: %d\n", h);
 #endif
-	return b_movable - w_movable;
+	return h;
 }
